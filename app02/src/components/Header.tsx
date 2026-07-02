a205fa76a5295acf2479e6cff37426f4c55dcf73
Vamsy <a.vamc.it@gmail.com>
2026-07-02 10:33:32 +0530

    nav links

--- app02/src/components/Header.tsx
+++ app02/src/components/Header.tsx
@@ -1,9 +1,17 @@
-import { Container, Navbar, NavbarBrand } from "react-bootstrap";
+import { Container, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle, NavLink } from "react-bootstrap";
 
 const Header = ({title}:{title:string}) => (
-    <Navbar expand="sm" className="bg-dark navbar-dark">
+    <Navbar expand="sm" className="bg-dark navbar-dark" collapseOnSelect>
         <Container>
             <NavbarBrand href="/">{title}</NavbarBrand>
+
+            <NavbarToggle aria-controls="adb-top-nav-bar" />
+
+            <NavbarCollapse id="adb-top-nav-bar">
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/contactUs">Contact Us</NavLink>
+                <NavLink href="/aboutUs">About Us</NavLink>
+            </NavbarCollapse>
         </Container>
     </Navbar>
 );
